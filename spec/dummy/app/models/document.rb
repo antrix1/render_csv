2791a6ea312d29329f8cbf225cf5374a742cd503
Andrej Dragojevic <andrejdr.93@gmail.com>
2016-01-17 23:48:44 +0100

    config paperclip and csv content type for integration test

--- spec/dummy/app/models/document.rb
+++ spec/dummy/app/models/document.rb
@@ -1,4 +1,4 @@
 class Document < ActiveRecord::Base
   has_attached_file :csv_file
-  validates_attachment :csv_file, content_type: { content_type: "text/csv" }
+  validates_attachment :csv_file, content_type: { content_type: ["text/csv", "text/comma-separated-values", "application/vnd.ms-excel"] }
 end
--- spec/integration/csv_delivery_spec.rb
+++ spec/integration/csv_delivery_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe "CSV Delivery", type: :request do
   end
 
   it "renders a csv table" do
+    visit new_document_path
+    attach_file 'document[csv_file]', "#{Rails.root}/csv_render.csv"
+    click_button "Upload"
+
+    expect(current_path).to match(/documents\/\d/)
+    expect(page).to have_selector :table
   end
 end
